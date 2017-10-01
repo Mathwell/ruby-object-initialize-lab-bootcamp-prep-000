4c3f4669dd71548e7195b58e42fe8e93bc398e8e
Olena Ageyeva <eageeva@hotmail.com>
2017-10-01 18:53:20 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+def initialize(dogname)
+  @name=dogname
+end
+  def name
+      @name
+    end
+
+    def name=(new_name)
+      @name = new_name
+    end
+
+
+  def breed
+    @breed
+  end
+
+  def breed=(dogbreed)
+    @breed=dogbreed
+  end
+
+end
